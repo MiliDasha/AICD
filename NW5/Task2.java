38455ae4e57a0e4b1a739fda895c4d04bb999011
MiliDasha <dashatarelova@gmail.com>
2026-04-02 22:13:01 +0300

    Add files via upload

---
+++ NW5/Task2.java
@@ -0,0 +1,11 @@
+package NW5;
+
+import java.util.Arrays;
+
+public class Task2 {
+    public static void main(String[] args) {
+        String[] arr = {"персик", "банан", "яблоко", "апельсин", "клубника"};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
